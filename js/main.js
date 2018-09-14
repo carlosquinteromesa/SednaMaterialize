cdfb6c28076bd53705e958742e96f8d7b3d6c050
carlosquinteromesa <carlosquin060400@gmail.com>
2018-09-14 11:33:14 -0500

    cambios Sedna

--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     })
   })
+  
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel') 
+  var arrowright = document.querySelector('.arrowright')
+  var arrowleft = document.querySelector('.arrowleft')
+  var instance = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true
+  })
+  
+  arrowright.addEventListener('click', () => {
+    var instances = M.Carousel.getInstance(elems[0])
+    instances.next()
+  })
+  arrowleft.addEventListener('click', () => {
+    var instances = M.Carousel.getInstance(elems[0])
+    instances.prev()
+  })
+})
+
